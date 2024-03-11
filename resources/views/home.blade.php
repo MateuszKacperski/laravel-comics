09e74f2032671c7934658e8120aaf99765577452
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-03-11 10:41:38 +0100

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,16 @@
    @section('main-content')
      <main class="container">
         <section id="products">
-            @foreach($comics as $comic)
+            @foreach ($comics=config('comics') as $index => $comic)  
             <div class="card">
+                <a href="{{route('comic', $index)}}">
                 <div class="product-card">
                     <div class="card-conteiner">
                         <img src="{{ $comic['thumb']}}" alt="{{ $comic['title']}}">
                         <h4>{{ $comic['title']}}</h4>
                     </div>
                 </div>
+            </a>
             </div>
             @endforeach
         </section>
